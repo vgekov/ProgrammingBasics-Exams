815c8cba9eb01530395d02ef0be034bc235aa31b
VGekov <vladislav.gekov@gmail.com>
2017-05-02 21:47:38 +0300

    task

--- Programming Basics Exam - 18 March 2017/03.CarToGo/Program.cs
+++ Programming Basics Exam - 18 March 2017/03.CarToGo/Program.cs
@@ -10,6 +10,74 @@ namespace _03.CarToGo
     {
         static void Main(string[] args)
         {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            #region Budget 100
+
+            if (budget <= 100)
+            {
+                if (season == "Summer")
+                {
+                    budget = budget * 0.35;
+                    Console.WriteLine("Economy class");
+                    Console.WriteLine("Cabrio - {0:f2}", budget);
+                }
+                else if (season == "Winter")
+                {
+                    budget = budget * 0.65;
+                    Console.WriteLine("Economy class");
+                    Console.WriteLine("Jeep - {0:f2}", budget);
+                }
+            }
+
+            #endregion
+
+            #region budget 100 500
+
+            else if (budget > 100 && budget <= 500)
+            {
+                if (season == "Summer")
+                {
+                    budget = budget * 0.45;
+                    Console.WriteLine("Compact class");
+                    Console.WriteLine("Cabrio - {0:f2}", budget);
+                }
+                else if (season == "Winter")
+                {
+                    budget = budget * 0.80;
+                    Console.WriteLine("Compact class");
+                    Console.WriteLine("Jeep - {0:f2}", budget);
+                }
+
+            }
+
+            #endregion
+
+            #region  over 500
+
+            else if (budget > 500)
+            {
+                if (season == "Summer")
+                {
+                    budget = budget * 0.90;
+                    Console.WriteLine("Luxury class");
+                    Console.WriteLine("Cabrio - {0:f2}", budget);
+                }
+                else if (season == "Winter")
+                {
+                    budget = budget * 0.90;
+                    Console.WriteLine("Luxury class");
+                    Console.WriteLine("Jeep - {0:f2}", budget);
+                }
+
+            }
+
+
+            #endregion
+
+
         }
     }
 }
+
