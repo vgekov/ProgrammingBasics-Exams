581df4822061ece797bf60f2b8e861410a8b194b
VGekov <vladislav.gekov@gmail.com>
2017-05-02 19:04:21 +0300

    Add second task

--- Programming Basics Exam - 18 March 2017/02.WorkHours/Program.cs
+++ Programming Basics Exam - 18 March 2017/02.WorkHours/Program.cs
@@ -10,6 +10,26 @@ namespace _02.WorkHours
     {
         static void Main(string[] args)
         {
+            double workHours = double.Parse(Console.ReadLine());
+            double workers = double.Parse(Console.ReadLine());
+            double workDays = double.Parse(Console.ReadLine());
+
+            double works = (workers * workDays * 8) - workHours;
+
+
+            if (works > 0)
+            {
+                Console.WriteLine("{0} hours left", works);
+            }
+            else
+            {
+
+                double penalties = works * workDays;
+                Console.WriteLine("{0} overtime", Math.Abs(works));
+                Console.WriteLine("Penalties: {0}", Math.Abs(penalties));
+
+            }
+
         }
     }
 }
